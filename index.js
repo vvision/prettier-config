f0183bb2eef3caf6b7c69f8fb7bf97ff312f27de
Victor Voisin <victor.voisin@unicoda.fr>
2018-05-14 19:41:12 +0200

    Testing first config

---
+++ index.js
@@ -0,0 +1,13 @@
+const prettierOptions = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: '<avoid'
+};
+
+module.exports = prettierOptions;
